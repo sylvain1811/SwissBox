d3029949de5e2ca44be2fa66e04599c29426acda
Rieben Axel <axel.rieben@he-arc.ch>
2017-10-26 11:36:52 +0200

    Start camera preview for the mirror module

---
+++ app/src/main/java/ch/hearc/swissbox/mirror/MirrorActivity.java
@@ -0,0 +1,15 @@
+package ch.hearc.swissbox.mirror;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ch.hearc.swissbox.R;
+
+public class MirrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mirror);
+    }
+}
