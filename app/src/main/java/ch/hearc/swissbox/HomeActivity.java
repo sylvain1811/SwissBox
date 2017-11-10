507e4e24b79fe4061e03f322a89557b67a93ccbc
Rieben Axel <axel.rieben@he-arc.ch>
2017-11-10 11:27:53 +0100

    Creating basic UI for dice module

--- app/src/main/java/ch/hearc/swissbox/HomeActivity.java
+++ app/src/main/java/ch/hearc/swissbox/HomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import ch.hearc.swissbox.dice.DiceActivity;
 import ch.hearc.swissbox.mirror.MirrorActivity;
 
 public class HomeActivity extends AppCompatActivity {
@@ -12,10 +13,17 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        launchDice();
     }
 
     private void launchMirror() {
         Intent intent = new Intent(this, MirrorActivity.class);
+        startActivity(intent);
+    }
+
+    private void launchDice() {
+        Intent intent = new Intent(this, DiceActivity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/ch/hearc/swissbox/dice/DiceActivity.java
@@ -0,0 +1,16 @@
+package ch.hearc.swissbox.dice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ch.hearc.swissbox.R;
+
+public class DiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice);
+
+    }
+}
