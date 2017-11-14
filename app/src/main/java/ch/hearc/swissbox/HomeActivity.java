0e1a269554a4f9fc7b103d6acbccf6d80d1f6d95
sylvain.renaud <sylvain.renaud@he-arc.ch>
2017-11-14 14:02:48 +0100

    Update home activity : one button to launch notepad, one to launch mirror.

--- app/src/main/java/ch/hearc/swissbox/HomeActivity.java
+++ app/src/main/java/ch/hearc/swissbox/HomeActivity.java
@@ -12,18 +12,22 @@ import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private View.OnClickListener btnClickListener = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        initBtnListener();
+
         Button btnNotepad = (Button) findViewById(R.id.btn_open_notepad);
-        btnNotepad.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            launchNotepad();
-            }
-        });
+        Button btnMirror = (Button) findViewById(R.id.btn_open_mirror);
+
+        if (btnClickListener != null) {
+            btnNotepad.setOnClickListener(btnClickListener);
+            btnMirror.setOnClickListener(btnClickListener);
+        }
     }
 
     private void launchMirror() {
@@ -34,5 +38,22 @@ public class HomeActivity extends AppCompatActivity {
     private void launchNotepad() {
         Intent intent = new Intent(HomeActivity.this, NotePadActivity.class);
         startActivity(intent);
+    }
+
+
+    private void initBtnListener() {
+        btnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btn_open_notepad:
+                        launchNotepad();
+                        break;
+                    case R.id.btn_open_mirror:
+                        launchMirror();
+                        break;
+                }
+            }
+        };
     }
 }
