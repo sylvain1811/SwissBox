a51b0c6b873f8499544e1117c797aa2a2088f0a1
johnny.dacosta <john.dacost@gmail.com>
2017-11-17 09:19:50 +0100

    Add flashlight to homeactivity

--- app/src/main/java/ch/hearc/swissbox/HomeActivity.java
+++ app/src/main/java/ch/hearc/swissbox/HomeActivity.java
@@ -1,9 +1,17 @@
 package ch.hearc.swissbox;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
+import ch.hearc.swissbox.flashlight.FlashLight;
 import ch.hearc.swissbox.notepad.NotePadActivity;
 import ch.hearc.swissbox.mirror.MirrorActivity;
 
@@ -18,6 +26,23 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        /**
+         * Get the instance FlashLight
+         */
+        final FlashLight flashLight = FlashLight.getInstance();
+        flashLight.setFlashAvailable(getApplicationContext()
+                .getPackageManager()
+                .hasSystemFeature(PackageManager
+                        .FEATURE_CAMERA_FLASH));
+
+        flashLight.setCameraManager((CameraManager) getSystemService(Context.CAMERA_SERVICE));
+
+        try {
+            flashLight.setCameraId(flashLight.getCameraManager().getCameraIdList()[0]);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
 
         /**
          * Target the different card view
@@ -49,17 +74,76 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+        cardFlashLight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!flashLight.isFlashAvailable()) {
+                    AlertBox();
+                    return;
+                }
+
+                try {
+                    if (flashLight.isFlashOn()) {
+                        turnOffFlashLight(flashLight);
+                        flashLight.setFlashOn(false);
+                    } else {
+                        turnOnFlashLight(flashLight);
+                        flashLight.setFlashOn(true);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
-    private void launchMirror() {
-        Intent intent = new Intent(this, MirrorActivity.class);
-        startActivity(intent);
+    /**
+     * Alert the user that the flash is not available on his device
+     */
+    private void AlertBox() {
+        AlertDialog alert = new AlertDialog.Builder(this)
+                .create();
+        alert.setTitle("Error !!");
+        alert.setMessage("Your device doesn't support flash light!");
+        alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // closing the application
+                        finish();
+                        System.exit(0);
+                    }
+                });
+        alert.show();
+        return;
     }
 
-    private void launchNotepad() {
-        Intent intent = new Intent(HomeActivity.this, NotePadActivity.class);
-        startActivity(intent);
+    /**
+     * Set the flash mode ON
+     */
+    public void turnOnFlashLight(FlashLight fl) {
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                fl.getCameraManager().setTorchMode(fl.getCameraId(), true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
+    /**
+     * Set the flash mode OFF
+     */
+    public void turnOffFlashLight(FlashLight fl) {
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                fl.getCameraManager().setTorchMode(fl.getCameraId(), false);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/ch/hearc/swissbox/flashlight/FlashLight.java
+++ app/src/main/java/ch/hearc/swissbox/flashlight/FlashLight.java
@@ -2,15 +2,91 @@ package ch.hearc.swissbox.flashlight;
 
 /**
  * Created by johnny.dacosta on 14/11/2017.
+ * <p>
+ * This was inspired by the tutorial from codeproject.com with the new Camera2 api
+ * https://www.codeproject.com/articles/1112813/android-flash-light-application-tutorial-using-cam</p>
  */
 
-class FlashLight {
-    private static final FlashLight ourInstance = new FlashLight();
 
-    static FlashLight getInstance() {
-        return ourInstance;
+import android.hardware.camera2.CameraManager;
+import android.media.MediaPlayer;
+import android.widget.Button;
+
+/**
+ * Pattern : singleton
+ */
+public class FlashLight {
+    private static FlashLight INSTANCE = null;
+
+    private Button switchLight;
+    private CameraManager cameraManager;
+    private String cameraId;
+    private boolean isFlashOn;
+    private boolean isFlashAvailable;
+    private MediaPlayer mp;
+
+    public static FlashLight getInstance() {
+
+        if (INSTANCE == null) {
+            INSTANCE = new FlashLight();
+        }
+        return INSTANCE;
     }
 
     private FlashLight() {
+        this.isFlashOn = false;
+        this.isFlashAvailable = false;
+        this.cameraManager = null;
+        this.switchLight = null;
+        this.mp = null;
+        this.cameraId = null;
+    }
+
+    public Button getSwitchLight() {
+        return switchLight;
+    }
+
+    public void setSwitchLight(Button switchLight) {
+        this.switchLight = switchLight;
+    }
+
+    public CameraManager getCameraManager() {
+        return cameraManager;
+    }
+
+    public void setCameraManager(CameraManager cameraManager) {
+        this.cameraManager = cameraManager;
+    }
+
+    public String getCameraId() {
+        return cameraId;
+    }
+
+    public void setCameraId(String cameraId) {
+        this.cameraId = cameraId;
+    }
+
+    public boolean isFlashOn() {
+        return isFlashOn;
+    }
+
+    public void setFlashOn(boolean flashOn) {
+        isFlashOn = flashOn;
+    }
+
+    public boolean isFlashAvailable() {
+        return isFlashAvailable;
+    }
+
+    public void setFlashAvailable(boolean flashAvailable) {
+        isFlashAvailable = flashAvailable;
+    }
+
+    public MediaPlayer getMp() {
+        return mp;
+    }
+
+    public void setMp(MediaPlayer mp) {
+        this.mp = mp;
     }
 }
